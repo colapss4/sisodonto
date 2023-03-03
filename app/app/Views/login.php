d8193e21e1992083ff1bf35965fd194eee8d0f5f
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2023-03-03 01:20:54 -0400

    setup: template structure

--- app/app/Views/login.php
+++ app/app/Views/login.php
@@ -1,1 +1,17 @@
+<?= $this->extend('Layout/main.php') ?>
+
+<?= $this->section('title') ?>
+
+<? if (isset($title)) {
+    echo $title . ' - ' . env('APP_NAME');
+} else {
+    echo env('APP_NAME');
+} ?>
+
+<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
+
 <h1>Login</h1>
+
+<?= $this->endSection() ?>
