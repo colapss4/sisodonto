b5b2ecd051ee2a74a7dc25289152358b0f0ef3fc
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2023-03-04 04:27:22 -0400

    add: clinic controller

--- app/app/Controllers/Web/Login.php
+++ app/app/Controllers/Web/Login.php
@@ -3,6 +3,7 @@
 namespace App\Controllers\Web;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
 
 class Login extends BaseController
 {
@@ -27,7 +28,29 @@ class Login extends BaseController
 
         $post = $this->request->getPost(['email', 'senha']);
 
-        $session->setFlashdata('error', implode(',', $post));
-        return view('login',  ['title' => $this->title]);
+        if (empty($post['email']) || empty($post['senha'])) {
+            $session->setFlashdata('error', 'Email ou senha não informado.');
+            return view('login',  ['title' => $this->title]);
+        }
+
+        $model = model(UserModel::class);
+
+        $user = $model->asObject()
+            ->where('email', $post['email'])
+            ->where('password', md5($post['senha']))
+            ->first();
+
+        if (is_null($user)) {
+            $session->setFlashdata('error', 'Usuário não encontrado.');
+            return view('login',  ['title' => $this->title]);
+        }
+
+        $session->set('user_logged', [
+            'user_id' => $user->id,
+            'user_name' => $user->name,
+            'user_email' => $user->email
+        ]);
+
+        return redirect()->to('/clinics');
     }
 }
--- app/routes/web.php
+++ app/routes/web.php
@@ -5,3 +5,4 @@ $routes->post('/login', 'Web\Login::authenticate', ['as' => 'login.post']);
 $routes->get('/register', 'Web\Register::index', ['as' => 'register']);
 $routes->post('/register', 'Web\Register::register', ['as' => 'register.post']);
 $routes->get('/reset', 'Web\Reset::index', ['as' => 'reset']);
+$routes->get('/clinics', 'Web\Clinics::index', ['as' => 'clinics']);
